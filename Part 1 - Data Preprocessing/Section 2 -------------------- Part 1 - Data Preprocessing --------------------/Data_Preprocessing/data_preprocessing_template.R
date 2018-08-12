b78ee28e138e488c20ef1e4508930b5f7743561c
Milu234 <milanhazra234.mh@gmail.com>
2018-08-12 09:29:14 +0530

    R mean done

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/data_preprocessing_template.R
@@ -3,6 +3,11 @@
 # Importing the dataset
 dataset = read.csv('Data.csv')
 
+
+#Take care of the missing values
+dataset$Age = ifelse(is.na(dataset$Age),ave(dataset$Age,FUN = function(X) mean(X,na.rm = TRUE)),dataset$Age) 
+dataset$Salary = ifelse(is.na(dataset$Salary),ave(dataset$Salary,FUN = function(X) mean(X,na.rm = TRUE)),dataset$Salary) 
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 
