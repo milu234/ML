f9437d4a8dd5736f585db272174a6168c72b6787
Milu234 <milanhazra234.mh@gmail.com>
2018-08-12 08:14:32 +0530

    import libraries and Dataset

---
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/data_preprocessing_template.R
@@ -0,0 +1,12 @@
+# Data Preprocessing Template
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+
+
+# Feature Scaling
+# training_set = scale(training_set)
+# test_set = scale(test_set)
