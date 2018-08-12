62750a433f000b2bd4251c9157facb7f50b2fbf7
Milu234 <milanhazra234.mh@gmail.com>
2018-08-12 09:59:05 +0530

    Changed the Country and Purcjased Column

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/data_preprocessing_template.py
@@ -1,13 +1,30 @@
+#import the librarires
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#Get the dataset 
 dataset = pd.read_csv('Data.csv')
 X  = dataset.iloc[:,:-1].values 
 Y = dataset.iloc[:,3].values
 
-
+#Enter the values of the missing table using the meanof that column
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN',strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+
+#Encoding /categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+
+onehotencoder =  OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
